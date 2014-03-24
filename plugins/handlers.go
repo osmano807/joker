1e2dd6ce655caa15b48b361ab4b8bc78bc076105
Joaquim Pedro França Simão (osmano807) <osmano807@gmail.com>
2014-03-24 16:26:44 -0300

    Adding license information

--- plugins/handlers.go
+++ plugins/handlers.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2013, 2014 The Joker Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package plugins
 
 import . "github.com/osmano807/joker/interfaces"
